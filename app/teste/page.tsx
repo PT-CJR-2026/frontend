2040114260f1484dae01cf7fd283717ace657ff6
Romm-0 <rafamelatti@gmail.com>
2026-05-24 11:49:35 -0300

    feat: modal base implementado

---
+++ app/teste/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import ProdutoBase from "@/app/produto/produto-base";
+
+export default function Home() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", backgroundColor: "#000000" }}>
+      <ProdutoBase/>
+    </div>
+  );
+}
