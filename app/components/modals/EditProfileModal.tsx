c62893989542af8cbd5ea371dcb9ad85de6026ba
Gabriel Severiano de Souza <gabriel.severianos@gmail.com>
2026-05-25 13:37:47 -0300

    style(Modals): reestilizando os icons key e de voltar para não ficar muito pequeno

--- app/components/modals/EditProfileModal.tsx
+++ app/components/modals/EditProfileModal.tsx
@@ -108,7 +108,7 @@ export function EditProfileModal({ onClose }: Props) {
           className="relative cursor-pointer"
           onClick={() => fileRef.current?.click()}
         >
-          <img
+          <img //avatarDo perfil
             src={avatarPreview || "/ion_person.svg"}
             alt="avatar"
             className="w-24 h-24 rounded-full object-cover bg-gray-300"
