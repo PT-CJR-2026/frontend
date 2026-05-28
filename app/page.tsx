13bb62a32b70351be579a678a34ccc3b785c373c
caiom <caiombechepeche@gmail.com>
2026-05-28 01:03:52 -0300

    feat: implementa card avaliação

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 'use client';
 
 import Navbar from "@/app/components/layout/Navbar";
-<<<<<<< HEAD
-import SearchBar from "./components/ui/SearchBar";
-=======
-import Hero from "./components/layout/Hero";
->>>>>>> f5dfbc961cbe80623c8a73e00540dd17c7ee1730
 
 export default function Home() {
   return (
@@ -13,16 +8,6 @@ export default function Home() {
       <Navbar
         logoSrc="/logo-branca-stock.io.svg"
       />
-<<<<<<< HEAD
-
-      {/* visualização teste da barra de pesquisa */}
-      <div className="w-full flex justify-center pt-12 px-4">
-        <SearchBar />
-      </div>
-
-=======
-      <Hero slug="home" />
->>>>>>> f5dfbc961cbe80623c8a73e00540dd17c7ee1730
     </div>
   );
 }
